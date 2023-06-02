f74bdd79397699ce2fb9eabbb3058f9937c836f5
Aditya Sharma <87620220+Adi-SRAM25@users.noreply.github.com>
2023-06-03 02:24:37 +0530

    Update FullAdder.v

--- FullAdder.v
+++ FullAdder.v
@@ -1,1 +1,15 @@
-https://github.com/Adi-SRAM25/Verilog/assets/87620220/ba5ecea3-0892-441f-81c2-d43d2deecbb3
+`timescale 1ns / 1ps
+
+module full_adder(
+    output cout,
+    output sum,
+    input ain,
+    input bin,
+    input cin
+    );
+
+    //assign bin2 = cin^bin; // XORing the inputs to bin with cin. If 1, subtract; if 0, add
+    
+    assign sum = ain^bin^cin;
+    assign cout = (ain&bin) | (ain&cin) | (bin&cin);
+endmodule

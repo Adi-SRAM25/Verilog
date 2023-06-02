fc7d55a991244d5773d6cdedf4ea4d883213920a
Aditya Sharma <87620220+Adi-SRAM25@users.noreply.github.com>
2023-06-03 02:34:31 +0530

    Update FSM_Mealy.v

--- FSM_Mealy.v
+++ FSM_Mealy.v
@@ -1,5 +1,29 @@
+module sm_Mealy(
+input clk,x,
+output [1:0] Sreg,
+output [1:0] Snext,
+output z);
+  
+reg [1:0] Sreg;
+reg z;
+reg [1:0] Snext;
+parameter [1:0] A =2'b00, B=2'b01, C=2'b10, D=2'b11; // define states
 
-always @ (x,Sreg) //output logic
+always @(posedge clk) // state memory
+Sreg<=Snext;
+  
+always @(x,Sreg) // next-state logic
+begin
+case(Sreg)
+A: if(x==0) Snext = A; else Snext = B; 
+B: if(x==0) Snext = B; else Snext = C;
+C: if(x==0) Snext = C; else Snext = D;
+D: if(x==0) Snext = D; else Snext = A;
+default Snext = A;
+endcase
+end
+  
+  always @ (x,Sreg) //output logic - Notice it's also dependent on input
 case(Sreg)
 A: if(x==0) z=0; else z=0;
 B: if(x==0) z=0; else z=0;
@@ -7,4 +31,5 @@ C: if(x==0) z=0; else z=0;
 D: if(x==0) z=0; else z=1;
 default z=0;
 endcase
+  
 endmodule

988999683536b30588c68529289e18b6755d0812
Aditya Sharma <87620220+Adi-SRAM25@users.noreply.github.com>
2023-06-03 02:33:24 +0530

    Update FSM_Mealy.v

--- FSM_Mealy.v
+++ FSM_Mealy.v
@@ -1,1 +1,10 @@
 
+always @ (x,Sreg) //output logic
+case(Sreg)
+A: if(x==0) z=0; else z=0;
+B: if(x==0) z=0; else z=0;
+C: if(x==0) z=0; else z=0;
+D: if(x==0) z=0; else z=1;
+default z=0;
+endcase
+endmodule
